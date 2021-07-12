64a95fe7abed9f555a8936809d0492a82fd123b4
francesco <franci.piazza@gmail.com>
2021-07-12 16:59:44 +0200

    New feature: added support for BMP images

--- version.h
+++ version.h
@@ -5,5 +5,5 @@
 #ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
 #define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
 
-const std::string VERSION = "1.0";
+const std::string VERSION = "1.1";
 #endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
