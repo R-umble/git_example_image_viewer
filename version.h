50b113f8bbb81b6759da2de2385f3b7ad1a98cc1
francesco <franci.piazza@gmail.com>
2021-07-12 16:45:57 +0200

    Rilascio v 1.0

---
+++ version.h
@@ -0,0 +1,9 @@
+//
+// Created by francesco on 12/07/21.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+const std::string VERSION = "1.0";
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
